4b7b70e5b017605a87fbe8e3d6078f0d72c9043e
Anatolii Kaliuzhnyi <anatolii.kaliuzhnyi@kohls.com>
2018-01-31 12:09:58 +0200

    test

---
+++ Pipeline.groovy
@@ -0,0 +1,41 @@
+class MavenBuild {
+    static job (dslFactory, jobConfig) {
+        def folderedBaseName
+        folderedBaseName = [
+                jobConfig.'folder.project'?: "",
+                jobConfig.'folder.jobType'?: "",
+                jobConfig.'job.baseName'].findAll { it != null && it.toString().length() != 0 }.join("/")
+                
+        def folderPath = folderedBaseName.tokenize('/').dropRight(1).join('/')
+        def list = folderPath.split("/").toList()
+        def folderName = "${list[0]}"
+        for (String item : list.drop(1)) {
+            folderName = folderName + "/" + item
+            dslFactory.folder(folderName)
+        }             
+
+        dslFactory.job(folderedBaseName) {
+            jobConfig."maven.profiles" = jobConfig."maven.profiles" + ["\$${jobConfig."job.profileParamName"}"]
+            parameters {
+                        stringParam(jobConfig.'maven.shaParamName', "", "SHA to checkout from")
+                        if (jobConfig.'maven.availableProfiles' && jobConfig.'job.profileParamName') {
+                            choiceParam(jobConfig.'job.profileParamName', jobConfig.'maven.availableProfiles', 'Please Select PROFILE')
+                        }
+                    }            
+            scm {
+                git {
+                    remote { 
+                        url("https://github.com/${jobConfig.'github.org'}/${jobConfig.'github.repo'}") 
+                        credentials('GitHub')
+                    }
+                    branch("\$${jobConfig.'maven.shaParamName'}")
+                }
+                definition {
+                    cps {
+                        script(dslFactory.jobManagement.readFileInWorkspace('pipeline.groovy'))
+                    }
+                }
+            }            
+        }
+    }
+}
---
+++ pipeline.groovy
@@ -0,0 +1,10 @@
+node(env.NODE_GROUP) {
+    stage('Initialization') {
+        deleteDir()
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+            userRemoteConfigs: [[url: 'https://github.com/ColFouks/CICD', credentialsId: "GitHub", branch: "master"]]])
+         def rawJC = readFile "${env.WORKSPACE}/test.jc"
+         JC = new groovy.json.JsonSlurperClassic().parseText(rawJC)
+    }
+    
+}
