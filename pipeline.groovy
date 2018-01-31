3d4a2b467ea0cc5ce8e53c4dbd55e159c581ea61
Anatolii Kaliuzhnyi <anatolii.kaliuzhnyi@kohls.com>
2018-01-31 16:27:08 +0200

    test

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,11 +1,22 @@
 node(env.NODE_GROUP) {
+    def JC
     stage('Initialization') {
         deleteDir()
         checkout([$class: 'GitSCM', branches: [[name: '*/master']],
             userRemoteConfigs: [[url: 'https://github.com/ColFouks/CICD', credentialsId: "GitHub", branch: "master"]]])
-         def rawJC = env.JC.decodeBase64()
+        def rawJC = env.JC.decodeBase64()
         rawJC = new String(rawJC)
-         JC = new ConfigSlurper().parse(rawJC)
+        JC = new groovy.json.JsonSlurperClassic().parseText(rawJC)
+    }
+    for (int i = 0; i < JC.pipeline.steps.size(); i++) {
+        def pipeJob = JC.pipeline.steps[i]
+
+        def pipelineParameters = []
+        for (int j; j < pipeJob.params.size(); j++){
+            pipelineParameters.add([$class: 'StringParameterValue', name: j.key, value: j.value])
+        }        
+        build job: pipeJob, parameters: pipelineParameters
+
     }
     
 }
